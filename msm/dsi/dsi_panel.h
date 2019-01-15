4d35779ea72e1327b8183389c13d6a7f574fe173
houdz1 <houdz1@motorola.com>
2019-01-15 17:34:44 +0800

    disp: msm: support for HBM_FOD mode
    FOD: Fingerprint of display.
    
    For tianma panel, there are two hbm mode.
    1. hbm
    2. hbm for FOD(hbm_fod);
    But for SMD panel ,there is only one HBM mode.
    
    Port from 9701d728f
    
    Change-Id: Icc09d39502a9549bfba7cd47efca2993a2716110
    Signed-off-by: houdz1 <houdz1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697595
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -226,6 +226,7 @@ enum acl_state {
 enum hbm_state {
 	HBM_OFF_STATE = 0,
 	HBM_ON_STATE,
+	HBM_FOD_ON_STATE,
 	HBM_STATE_NUM
 };
 
@@ -238,7 +239,7 @@ struct panel_param_val_map {
 struct panel_param {
 	const char *param_name;
 	struct panel_param_val_map *val_map;
-	const u16 val_max;
+	u16 val_max;
 	const u16 default_value;
 	u16 value;
 	bool is_supported;
@@ -329,6 +330,8 @@ struct dsi_panel {
 
 	u32 disp_on_chk_val;
 	bool no_panel_on_read_support;
+
+	bool panel_hbm_fod;
 
 	struct panel_param *param_cmds;
 };
