60b83689e184feb55ca356793a93d5b74010ceec
Bang Nguyen <bangnguyen@motorola.com>
2019-07-25 12:56:02 -0500

    disp: msm: dsi: parsing BL setting ESD and panel
    After the Boot Loader (BL) detects the panel information, or reading
    the ESD UTAG setting, the BL will write these information to the DTS.
    The display driver from kernel and parse and save these setting.
    
    Port from: 2701eb2
    
    Change-Id: Ibd1c9429854655ea7c80222ce64c3b86c6df2eda
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697267
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shuo Yan <shuoyan@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -41,6 +41,8 @@
 #define MIPI_DSI_MSG_BATCH_COMMAND BIT(6)
 #define MIPI_DSI_MSG_UNICAST_COMMAND BIT(7)
 
+#define DSI_PANEL_MAX_PANEL_LEN	256
+
 enum dsi_panel_rotation {
 	DSI_PANEL_ROTATE_NONE = 0,
 	DSI_PANEL_ROTATE_HV_FLIP,
@@ -320,6 +322,9 @@ struct dsi_panel {
 	bool post_power_enable_status;
 
 	struct dsi_panel_ops panel_ops;
+	bool esd_utag_enable;
+	u64 panel_ver;
+	char panel_name[DSI_PANEL_MAX_PANEL_LEN];
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
