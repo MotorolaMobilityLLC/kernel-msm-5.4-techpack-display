a6402f665e6ecaec90da6641ca67859db725a252
Bang Nguyen <bangnguyen@motorola.com>
2019-07-31 12:58:34 -0500

    disp: msm: add panel's param feature to display FW
    Add the display's param feature such as HBM, ACL, or CABC feature to
    display framework
    
    Port from: de5326b
    
    Change-Id: I42b8f1319786f21f79b2958f284351ff0ef3ae5f
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697380
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -271,6 +271,8 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_QSYNC_ON                   Enable qsync mode
  * @DSI_CMD_SET_QSYNC_OFF                  Disable qsync mode
  * @DSI_CMD_SET_CALIBRATION_DATA           Panel calibration data
+ * @DSI_CMD_SET_HBM_ON:                    Turning HBM on
+ * @DSI_CMD_SET_HBM_OFF:                   Turning HBM off
  * @DSI_CMD_SET_MAX
  */
 enum dsi_cmd_set_type {
@@ -300,6 +302,8 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_QSYNC_ON,
 	DSI_CMD_SET_QSYNC_OFF,
 	DSI_CMD_SET_CALIBRATION_DATA,
+	DSI_CMD_SET_HBM_ON,
+	DSI_CMD_SET_HBM_OFF,
 	DSI_CMD_SET_MAX
 };
 
