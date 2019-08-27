f87037851c94eac796a0416b7d54ea061c5238c0
Bang Nguyen <bangnguyen@motorola.com>
2019-08-27 09:19:45 -0500

    disp: msm : Add the ACL panel param feature
    Add the ACL panel param feature which will be controlled
    from the user-space
    
    Port from: 6db5516a8
    
    Change-Id: Ib286fbcbb7e1517bd6390d1338d157f371ce680f
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697573
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -286,6 +286,8 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_TRIGGER_SELF_REFRESH:      Trigger self refresh from Gram
  * @DSI_CMD_SET_HBM_ON:                    Turning HBM on
  * @DSI_CMD_SET_HBM_OFF:                   Turning HBM off
+ * @DSI_CMD_SET_ACL_ON:                    Turning ACL on
+ * @DSI_CMD_SET_ACL_OFF:                   Turning ACL off
  * @DSI_CMD_SET_MAX
  */
 enum dsi_cmd_set_type {
@@ -328,6 +330,8 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_TRIGGER_SELF_REFRESH,
 	DSI_CMD_SET_HBM_ON,
 	DSI_CMD_SET_HBM_OFF,
+	DSI_CMD_SET_ACL_ON,
+	DSI_CMD_SET_ACL_OFF,
 	DSI_CMD_SET_MAX
 };
 
