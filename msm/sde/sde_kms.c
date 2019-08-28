c0127cea0bf7412e627310b85d1a41c8b18c5dfc
Bang Nguyen <bangnguyen@motorola.com>
2019-08-28 14:02:15 -0500

    disp: msm: Add DispUtil tool to motUtil Debugfs
    Add the Display Utility(DispUtil) support to the motUtil Debugfs Framework.
    This dispUtil tool will send a command from the motUtil debugfs to the panel
    or it can read any information from panel back.
    The 1st byte (set to 0) will indicate for the displUtil tool.
    
    DispUtil Format:
    
    1st         2nd         3rd    4th  5th       6th
    0=DispUtil  0=Pri Disp 0=write 0=HS 0=DCS     number byte to read
                1=Sec Disp 1=read  1=LP 1=Generic
    
    7th        8th         9th....
    payldLen_u payldLen_l  payload
    
    Port from: 890aa1c9
    
    Change-Id: I4ebba9f87dadcc3e9c22ddb58b4412c7124d7895
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697321
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_kms.c
+++ msm/sde/sde_kms.c
@@ -31,6 +31,9 @@
 #include <drm/drm_atomic_uapi.h>
 #include <drm/drm_probe_helper.h>
 #include <linux/version.h>
+#include <linux/memblock.h>
+#include <linux/string.h>
+#include <linux/slab.h>
 
 #include "msm_drv.h"
 #include "msm_mmu.h"
@@ -68,6 +71,8 @@
 #ifdef CONFIG_DRM_SDE_VM
 #include <linux/gunyah/gh_irq_lend.h>
 #endif
+
+#include "sde_motUtil.h"
 
 #define CREATE_TRACE_POINTS
 #include "sde_trace.h"
@@ -1896,6 +1901,7 @@ static int _sde_kms_setup_displays(struct drm_device *dev,
 		.post_kickoff = dsi_conn_post_kickoff,
 		.check_status = dsi_display_check_status,
 		.enable_event = dsi_conn_enable_event,
+		.motUtil_transfer = dsi_display_motUtil_transfer,
 		.cmd_transfer = dsi_display_cmd_transfer,
 		.cont_splash_config = dsi_display_cont_splash_config,
 		.cont_splash_res_disable = dsi_display_cont_splash_res_disable,
