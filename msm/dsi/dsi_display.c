b3bb1db94666b845ae397c4d114a3dac4798a08b
zhanggb <zhanggb@motorola.com>
2021-08-23 15:53:03 +0800

    Kernel: Add Moto extern display code
    [1] read panel mipi command(change to sys/class/drm)
    Use below command to get current dsi panel command:
    adb shell "cat /sys/class/drm/card0-DSI-1/dsi_display_parse_para"
    
    [2]Debug tool - parse/update lcd para from data file
    Add a parse function to get lcd bringup parameters from file in
    /vendor/firmware/ OR /data/vendor/param/display/. This help vendor
    engineer to debug lcd issue without requiring moto engineer to
    bulid new dtbo or boot image, it is also used by ParamUpdater app.
      1.Please push lcd para file lcd-paras.txt to /vendor/firmware/
        or /data/vendor/param/display/;
      2.Below command can update lcd paras:
        adb shell "echo num > /sys/class/drm/card0-DSI-1/dsi_display_parse_para"
    
    Progagate from below changeID:
    Ifefe7706b3f5d9c6f6aa11e26282e94a053b1ae4
    I6483b06b1be2bd51a5e8f49b72a476f5f19f0275
    Icc6578f4cade1acbd3ea09076128fca397600a89
    
    Change-Id: Ib90d4a6af8c34a55d9afd0688f995777c8c32c96
    Signed-off-by: zhanggb <zhanggb@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2048596
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -27,6 +27,7 @@
 #include "dsi_parser.h"
 #include "dsi_display_manager.h"
 #include "dsi_hfi.h"
+#include "dsi_display_mot_ext.h"
 
 #define to_dsi_display(x) container_of(x, struct dsi_display, host)
 #define INT_BASE_10 10
@@ -7092,6 +7093,7 @@ int dsi_display_dev_probe(struct platform_device *pdev)
 		goto end;
 
 	moto_panel_sysfs_add(display);
+	dsi_display_ext_init(display);
 
 	return 0;
 end:
