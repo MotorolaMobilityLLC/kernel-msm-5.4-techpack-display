bdd36f85d2591052b6c61f228650c580523c4071
tianpan Chen <chentp1@motorola.com>
2022-08-10 14:57:46 +0800

    display read 8s id
    modified display document:
    modified display read 8s id
    
    Change-Id: I373cb5ee3366f4af90209d31557cc7d5c95a6828
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2355363
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display.h
+++ msm/dsi/dsi_display.h
@@ -34,6 +34,8 @@
 #define DSI_MODE_MATCH_FULL_TIMINGS (DSI_MODE_MATCH_ACTIVE_TIMINGS | DSI_MODE_MATCH_PORCH_TIMINGS)
 #define DSI_MODE_MATCH_DSC_CONFIG (1 << 2)
 
+#define MAX_PANEL_CELLID      50
+
 /*
  * DSI Validate Mode modifiers
  * @DSI_VALIDATE_FLAG_ALLOW_ADJUST:	Allow mode validation to also do fixup
@@ -307,6 +309,8 @@ struct dsi_display {
 
 	bool enabled;
 	bool sysfs_add_done;
+	u8 cellid[MAX_PANEL_CELLID];
+	bool read_cellid;
 };
 
 int dsi_display_dev_probe(struct platform_device *pdev);
@@ -875,6 +879,9 @@ int dsi_display_restore_bit_clk(struct dsi_display *display, struct dsi_display_
  */
 bool dsi_display_mode_match(const struct dsi_display_mode *mode1,
 		struct dsi_display_mode *mode2, unsigned int match_flags);
+
+void dsi_display_set_cmd_tx_ctrl_flags(struct dsi_display *display,
+		struct dsi_cmd_desc *cmd);
 
 /**
  * dsi_display_update_transfer_time() - update DSI transfer time and clocks
