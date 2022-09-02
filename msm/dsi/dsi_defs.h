c8139398dab7954329cb9652605a90eb14ac8c95
houdz1 <houdz1@lenovo.com>
2022-09-02 15:26:50 +0800

    msm/display-drivers: suppor for reading 8s code.
    Get requirtments from fatory.
    Read 8s code from the register of panel IC.
    
    Change-Id: I8cc58dd95b51fc01d5f5f852c45bb023818dab67
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2678947
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -291,6 +291,7 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_DFPS_CMD_90:                DFPS switch to 90
  * @DSI_CMD_SET_DFPS_CMD_120:               DFPS switch to 120
  * @DSI_CMD_SET_DFPS_CMD_144:               DFPS switch to 144
+ * @DSI_CMD_SET_PANEL_CELLID:               Panel cellid
  * @DSI_CMD_SET_MAX
  */
 enum dsi_cmd_set_type {
@@ -340,6 +341,7 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_DFPS_CMD_90,
 	DSI_CMD_SET_DFPS_CMD_120,
 	DSI_CMD_SET_DFPS_CMD_144,
+	DSI_CMD_SET_PANEL_CELLID,
 	DSI_CMD_SET_MAX
 };
 
