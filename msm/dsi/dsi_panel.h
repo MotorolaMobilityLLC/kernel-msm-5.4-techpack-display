2881be40fe3daafc477435d059497d42c0fff10e
houdz1 <houdz1@lenovo.com>
2022-09-02 15:26:50 +0800

    msm/display-drivers: suppor for reading 8s code.
    Get requirtments from fatory.
    Read 8s code from the register of panel IC.
    
    Change-Id: I8cc58dd95b51fc01d5f5f852c45bb023818dab67
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2678947
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -235,6 +235,14 @@ struct drm_panel_esd_config {
 	u32 groups;
 };
 
+struct drm_panel_cellid_config {
+	bool cellid_enabled;
+
+	struct dsi_panel_cmd_set cellid_cmd;
+	u32 cellid_rlen;
+	u8 *return_buf;
+};
+
 struct dsi_panel_spr_info {
 	bool enable;
 	enum msm_display_spr_pack_type pack_type;
@@ -357,6 +365,8 @@ struct dsi_panel {
 	struct drm_panel_esd_config esd_config;
 
 	struct dsi_panel_lhbm_config lhbm_config;
+
+	struct drm_panel_cellid_config cellid_config;
 
 	struct dsi_parser_utils utils;
 
@@ -565,4 +575,5 @@ int dsi_panel_get_elvss_data_1(struct dsi_panel *panel);
 int dsi_panel_set_elvss_dim_off(struct dsi_panel *panel, u8 val);
 int dsi_panel_parse_elvss_config(struct dsi_panel *panel, u8 elv_vl);
 int dsi_panel_dfps_send_cmd(struct dsi_panel *panel);
+int dsi_panel_tx_cellid_cmd(struct dsi_panel *panel);
 #endif /* _DSI_PANEL_H_ */
