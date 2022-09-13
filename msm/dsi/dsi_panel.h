d7741896e632a8c8c499a5ec10b822779d049922
daiss1 <daiss1@lenovo.com>
2022-09-13 15:17:05 +0800

    drm/display: Add DC mode status sys interface
    Modify:
    Add DC mode status sys interface
    sensor get this status
    
    Change-Id: Ie4dfcacd7fc4a05ba28819e7c31d2afedcc29445
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2383947
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -433,6 +433,7 @@ struct dsi_panel {
 	bool dc_on;
 	bool mot_nt37701A_read_cellid;
 	bool esd_first_check;
+	int dc_state;
 };
 
 bool dsi_display_all_displays_dead(void);
@@ -589,3 +590,4 @@ int dsi_panel_dfps_send_cmd(struct dsi_panel *panel);
 int dsi_panel_tx_cellid_cmd(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
+
