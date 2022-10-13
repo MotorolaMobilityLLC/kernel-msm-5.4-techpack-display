285f89ac01cf026acedd5a112c21dddbaaa24814
zhanggb <zhanggb@motorola.com>
2022-10-13 23:25:05 +0800

    drm: Add display mode group
    LTPO panel can produce different display mode with same framerate. And there
    is limitation between certain display modes. So we need put proper display
    modes to different display mode group.
    As struct drmModeModeInfo can not be modified and the bit definition of
    its flags and type are almost used out, I have to use it name str to transfer
    group flag to Hal and then to SurfaceFlinger.
    
    Change-Id: Iea6266f5994103fca2e8790596af8462125ae6aa
    Reviewed-on: https://gerrit.mot.com/2403510
    Reviewed-on: https://gerrit.mot.com/2734040
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -524,6 +524,8 @@ struct dsi_mode_info {
 	bool esync_enabled;
 	u32 esync_emsync_fps;
 	u32 te_pulse_width_us;
+	// Motorola zhanggb, add refreshrate group, IKSWT-18219
+	u32 refresh_rate_group_flag;
 };
 
 /**
