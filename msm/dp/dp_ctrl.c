81e667d8452219b9d434c40642f9cea166e367da
Guobin Zhang <zhanggb@motorola.com>
2022-11-18 15:33:12 +0800

    Revert "disp: msm: dp: add debug node to capture source and sink crc"
    Why:
    With certain Skyworth TV and CableCreation C-HDMI cable, there are screen
    abnormal issue when connect phone and monitor on 8550 platform. This issue
    dissappear after revert this patch.
    
    Mot-CRs-Fixed: (CR)
    
    This reverts commit 7eef92843d7ef8c956bdd51f0440950cc04478dd.
    
    Change-Id: Ic70382f2230708e165ba6fb0fdcca338e5f45b35
    Reviewed-on: https://gerrit.mot.com/2433514
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Submit-Approved: Jira Key

--- msm/dp/dp_ctrl.c
+++ msm/dp/dp_ctrl.c
@@ -1529,30 +1529,6 @@ void dp_ctrl_set_sim_mode(struct dp_ctrl *dp_ctrl, bool en)
 	DP_INFO("sim_mode=%d\n", ctrl->sim_mode);
 }
 
-int dp_ctrl_setup_misr(struct dp_ctrl *dp_ctrl)
-{
-	struct dp_ctrl_private *ctrl;
-
-	if (!dp_ctrl)
-		return -EINVAL;
-
-	ctrl = container_of(dp_ctrl, struct dp_ctrl_private, dp_ctrl);
-
-	return ctrl->catalog->setup_misr(ctrl->catalog);
-}
-
-int dp_ctrl_read_misr(struct dp_ctrl *dp_ctrl, struct dp_misr40_data *data)
-{
-	struct dp_ctrl_private *ctrl;
-
-	if (!dp_ctrl)
-		return -EINVAL;
-
-	ctrl = container_of(dp_ctrl, struct dp_ctrl_private, dp_ctrl);
-
-	return ctrl->catalog->read_misr(ctrl->catalog, data);
-}
-
 struct dp_ctrl *dp_ctrl_get(struct dp_ctrl_in *in)
 {
 	int rc = 0;
@@ -1603,8 +1579,6 @@ struct dp_ctrl *dp_ctrl_get(struct dp_ctrl_in *in)
 	dp_ctrl->stream_pre_off = dp_ctrl_stream_pre_off;
 	dp_ctrl->set_mst_channel_info = dp_ctrl_set_mst_channel_info;
 	dp_ctrl->set_sim_mode = dp_ctrl_set_sim_mode;
-	dp_ctrl->setup_misr = dp_ctrl_setup_misr;
-	dp_ctrl->read_misr = dp_ctrl_read_misr;
 
 	return dp_ctrl;
 error:
