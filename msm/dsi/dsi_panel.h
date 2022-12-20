80703fcf48b73c697718f7123e96f00f09f99391
daiss1 <daiss1@lenovo.com>
2022-12-20 17:09:42 +0800

    msm/drm: Juno According to the basic refresh rate, switch 30/10/1hz
    Juno According to the basic refresh rate, switch 30/10/1hz
    30/10/1hz-->
    165/120/90/60/24/ base 120hz
    90 base 90hz
    
    Change-Id: I73bb86fab4d13f3b027b00a2cd32659c53986625
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2466248
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -370,6 +370,8 @@ struct dsi_panel {
 	struct dsi_display_mode *cur_mode;
 	u32 num_timing_nodes;
 	u32 num_display_modes;
+	u32 refresh_rate_base;
+	bool 	switch_rate_base;
 
 	struct dsi_regulator_info power_info;
 	struct dsi_backlight_config bl_config;
