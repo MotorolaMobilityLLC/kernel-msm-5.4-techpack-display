b3cfb6e0bbee8a682a91877d9f36ea029e374b67
Gaurav LNU <quic_glnu@quicinc.com>
2023-08-15 22:40:33 -0700

    disp: msm: sde: avoid double freeing of encoder ptr
    This change fixes the double freeing of physical
    encoder ptr which contains the same memory address
    in case of video or command mode usecase.
    
    Change-Id: Ic5704201785494d36142ec3065575f527e96475c
    Signed-off-by: Gaurav LNU <quic_glnu@quicinc.com>

--- msm/sde/sde_encoder.c
+++ msm/sde/sde_encoder.c
@@ -763,6 +763,7 @@ void sde_encoder_destroy(struct drm_encoder *drm_enc)
 			phys->ops.destroy(phys);
 			--sde_enc->num_phys_encs;
 			sde_enc->phys_vid_encs[i] = NULL;
+			sde_enc->phys_encs[i] = NULL;
 		}
 
 		phys = sde_enc->phys_cmd_encs[i];
@@ -770,6 +771,7 @@ void sde_encoder_destroy(struct drm_encoder *drm_enc)
 			phys->ops.destroy(phys);
 			--sde_enc->num_phys_encs;
 			sde_enc->phys_cmd_encs[i] = NULL;
+			sde_enc->phys_encs[i] = NULL;
 		}
 
 		phys = sde_enc->phys_encs[i];
