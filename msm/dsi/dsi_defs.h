82ab4393d6815eff7554ea3c93d1af883ffa3636
daiss1 <daiss1@lenovo.com>
2023-12-12 16:40:47 +0800

    display: Add apl moto command function
    qcom,apl-enabled flag apl enable
    qcom,mdss-dsi-panel-APL-THRESHOLD-BL  enable apl threshold
    qcom,mdss-dsi-panel-apl-on-command = [
    		39 00 00 00 00 00 03 5F 00 01];
    qcom,mdss-dsi-panel-apl-off-command = [
    		39 00 00 00 00 00 03 5F 00 00];
    Apl on/off command
    
    Change-Id: Icf911b3d6ea97777a219bcbbeb7d32130550c35b
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2822201
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -305,6 +305,8 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_DFPS_CMD_120:               DFPS switch to 120
  * @DSI_CMD_SET_DFPS_CMD_144:               DFPS switch to 144
  * @DSI_CMD_SET_PANEL_CELLID:               Panel cellid
+ * @DSI_CMD_SET_APL_ON:              apl on
+ * @DSI_CMD_SET_APL_OFF:               apl off
  * @DSI_CMD_SET_MAX
  */
 enum dsi_cmd_set_type {
@@ -366,6 +368,8 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_DFPS_CMD_120,
 	DSI_CMD_SET_DFPS_CMD_144,
 	DSI_CMD_SET_PANEL_CELLID,
+	DSI_CMD_SET_APL_ON,
+	DSI_CMD_SET_APL_OFF,
 	DSI_CMD_SET_MAX
 };
 
