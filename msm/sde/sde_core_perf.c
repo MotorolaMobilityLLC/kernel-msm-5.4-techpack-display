086adadd409375deae11609d4bdfd48f0fb25ba2
wangyq13 <wangyq13@lenovo.com>
2024-12-28 17:52:07 +0800

    sde: set threshold to 3Gbps to trigger MDP perf mode
    sde:
    Set threshold to 3Gbps, make it easier to trigger MDP perf mode.
    Make underrun hard to happen.
    
    Change-Id: Ic186690f584633d3c0c54d47b324ffddbdf0d19f
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3185628
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_core_perf.c
+++ msm/sde/sde_core_perf.c
@@ -757,7 +757,7 @@ static void _sde_core_perf_crtc_update_bus(struct sde_kms *kms,
 	bus_ab_quota = min(bus_ab_quota,
 			kms->catalog->perf.max_bw_high*1000ULL);
 
-	if (bus_ab_quota >= 4500000000ULL) {
+	if (bus_ab_quota >= 3000000000ULL) {
 		bus_ab_quota = max(bus_ab_quota, kms->catalog->perf.max_bw_high*1000ULL);
 		SDE_ERROR("change BW to Max value");
 	}
