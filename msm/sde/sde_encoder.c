30b1089e2f0a1953585d33fd4956653e8fe1604f
wangyq13 <wangyq13@lenovo.com>
2025-01-02 14:33:31 +0800

    sde: enable panic and print bw of display(NEED REVERT)
    sde:
    - still enable panic to monitor underrun
    - print bw of display
    
    Change-Id: I975493a5606dd1535124efc9b3cd086284036855
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3188558
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <lvxj4@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_encoder.c
+++ msm/sde/sde_encoder.c
@@ -3781,6 +3781,7 @@ static void sde_encoder_vblank_callback(struct drm_encoder *drm_enc,
 	SDE_ATRACE_END("encoder_vblank_callback");
 }
 
+extern u32 old_bw;
 static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 		struct sde_encoder_phys *phy_enc)
 {
@@ -3796,8 +3797,8 @@ static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 		sde_enc->cur_master->ops.get_underrun_line_count(
 				sde_enc->cur_master);
 
-	pr_warn("Underrun detected count:%d",
-                atomic_read(&phy_enc->underrun_cnt));
+	pr_warn("Underrun detected count:%d, bandwidth=%uk",
+                atomic_read(&phy_enc->underrun_cnt), old_bw);
 
 	trace_sde_encoder_underrun(DRMID(drm_enc),
 		atomic_read(&phy_enc->underrun_cnt));
@@ -3810,6 +3811,7 @@ static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 	SDE_DBG_CTRL("panic_underrun");
 
 	SDE_ATRACE_END("encoder_underrun_callback");
+	BUG();
 }
 
 void sde_encoder_register_vblank_callback(struct drm_encoder *drm_enc,
