7f8976fa8a802ba2aed6f600313beb477a63df09
wangyq13 <wangyq13@lenovo.com>
2025-01-02 13:50:09 +0800

    Revert "(CR) sde: trigger panic when underrun happen(NEED REVERT)"
    This reverts commit 724c6e5a3cebb40395f4b1c57f0e414b58ea977c.
    
    Mot-CRs-fixed: (CR)
    
    Change to 3Gbps to trigger perf mode, issue can not reproduce, so
    remove all the debug logs, only keep panic
    
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Change-Id: Ie2f08ef5b4a89312ff7be65f8b948c9ce8e54817
    Reviewed-on: https://gerrit.mot.com/3188481
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <lvxj4@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_encoder.c
+++ msm/sde/sde_encoder.c
@@ -3802,7 +3802,6 @@ static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 	SDE_DBG_CTRL("panic_underrun");
 
 	SDE_ATRACE_END("encoder_underrun_callback");
-	BUG();
 }
 
 void sde_encoder_register_vblank_callback(struct drm_encoder *drm_enc,
