9e2fd79c2e0058fe8253324251ede5c46ae87438
tianpan Chen <chentp1@motorola.com>
2025-01-17 16:03:26 +0800

    Aura25 display: add 165hz config for tm cli panel.
    Aura25 display:
    add 165hz interface config for tianma cli panel.
    
    Change-Id: Ibab3ca5844849212141a2299b48cd7a0a1ea8790
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3205780
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -291,6 +291,7 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_DFPS_CMD_90:                DFPS switch to 90
  * @DSI_CMD_SET_DFPS_CMD_120:               DFPS switch to 120
  * @DSI_CMD_SET_DFPS_CMD_144:               DFPS switch to 144
+ * @DSI_CMD_SET_DFPS_CMD_165:               DFPS switch to 165
  * @DSI_CMD_SET_PANEL_CELLID:               Panel cellid
  * @DSI_CMD_SET_APL_ON:              apl on
  * @DSI_CMD_SET_APL_OFF:               apl off
@@ -346,6 +347,7 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_DFPS_CMD_90,
 	DSI_CMD_SET_DFPS_CMD_120,
 	DSI_CMD_SET_DFPS_CMD_144,
+	DSI_CMD_SET_DFPS_CMD_165,
 	DSI_CMD_SET_PANEL_CELLID,
 	DSI_CMD_SET_APL_ON,
 	DSI_CMD_SET_APL_OFF,
