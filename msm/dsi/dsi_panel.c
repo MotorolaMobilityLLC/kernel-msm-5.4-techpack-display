e870291ba26ef66fdf654d39f95965eedeadd7de
daiss1 <daiss1@lenovo.com>
2025-01-20 17:02:55 +0800

    display: Add apl interface for Dolby disable apl
    Add apl interface for Dolby disable apl
    When highlighted,play dolby video and disable apl
    Fix pick error
    
    Change-Id: I48937b8fa486d126f78227278f873bc8f231c60a
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3207201
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -7239,12 +7239,6 @@ int dsi_panel_set_partition_refreshrate(struct dsi_panel *panel,
           }
 	mutex_unlock(&panel->panel_lock);
 
-       //Add dobly apl enable/display
-       if(mot_cmd->id == panel->apl_config.apl_funcid){
-               panel->apl_config.dobly_enable = mot_cmd->val;
-	        DSI_INFO("Update apl_config.dobly_enable = %d\n",panel->apl_config.dobly_enable);
-	}
-
 	return rc;
 }
 
@@ -7257,6 +7251,11 @@ int dsi_panel_set_moto_drm_command(struct dsi_panel *panel,
                 return -EINVAL;
         }
 	DSI_INFO("%s: %d %d\n", __func__, mot_cmd->id, mot_cmd->val);
+       //Add dobly apl enable/display
+       if(mot_cmd->id == panel->apl_config.apl_funcid){
+               panel->apl_config.dobly_enable = mot_cmd->val;
+	        DSI_INFO("Update apl_config.dobly_enable = %d\n",panel->apl_config.dobly_enable);
+	}
 
 	return rc;
 }
