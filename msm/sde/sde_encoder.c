828e6b94585e981a693d729011c775ad6c9be3fd
Yeqing Wang <wangyq13@motorola.com>
2025-02-05 01:29:17 -0600

    Revert "(CR) sde: enable panic and print bw of display(NEED REVERT)"
    This reverts commit 813f026db039a71ace9606d6f59ed19ae0ac58df.
    
    Mot-CRs-fixed: (CR)
    
    remove this panic, because we don't receive such panic after WA3
    
    Change-Id: Iaa6f86099c734f7bb98a01a714f545098a5b2a2e
    Reviewed-on: https://gerrit.mot.com/3224002
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_encoder.c
+++ msm/sde/sde_encoder.c
@@ -3781,7 +3781,6 @@ static void sde_encoder_vblank_callback(struct drm_encoder *drm_enc,
 	SDE_ATRACE_END("encoder_vblank_callback");
 }
 
-extern u32 old_bw;
 static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 		struct sde_encoder_phys *phy_enc)
 {
@@ -3797,8 +3796,8 @@ static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 		sde_enc->cur_master->ops.get_underrun_line_count(
 				sde_enc->cur_master);
 
-	pr_warn("Underrun detected count:%d, bandwidth=%uk",
-                atomic_read(&phy_enc->underrun_cnt), old_bw);
+	pr_warn("Underrun detected count:%d",
+                atomic_read(&phy_enc->underrun_cnt));
 
 	trace_sde_encoder_underrun(DRMID(drm_enc),
 		atomic_read(&phy_enc->underrun_cnt));
@@ -3811,7 +3810,6 @@ static void sde_encoder_underrun_callback(struct drm_encoder *drm_enc,
 	SDE_DBG_CTRL("panic_underrun");
 
 	SDE_ATRACE_END("encoder_underrun_callback");
-	BUG();
 }
 
 void sde_encoder_register_vblank_callback(struct drm_encoder *drm_enc,
