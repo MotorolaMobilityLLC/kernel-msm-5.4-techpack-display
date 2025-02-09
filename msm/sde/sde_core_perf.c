ced40732458d12ae8f73ab511276947c0c35e26f
wangyq13 <wangyq13@lenovo.com>
2025-02-09 21:57:54 +0800

    sde: change mdp perf boost threshold to 1.7Gbps
    sde:
    Change mdp boost perf mode to 1.7Gbps - Workaround 4
    
    Change-Id: Ie5256bd5ed83792d46a3b69b17a13acae7fafa5f
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3228627
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_core_perf.c
+++ msm/sde/sde_core_perf.c
@@ -760,7 +760,7 @@ static void _sde_core_perf_crtc_update_bus(struct sde_kms *kms,
 	if (bus_ab_quota >= 3000000000ULL) {
 		bus_ab_quota = max(bus_ab_quota, kms->catalog->perf.max_bw_high*1000ULL);
 		SDE_DEBUG("change BW to Max value");
-	} else if (bus_ab_quota > 2700000000ULL && bus_ab_quota < 3000000000ULL) {
+	} else if (bus_ab_quota > 1700000000ULL && bus_ab_quota < 3000000000ULL) {
 		bus_ab_quota = min(bus_ab_quota + 2000000000ULL, kms->catalog->perf.max_bw_high*1000ULL);
 		SDE_DEBUG("change BW to second level");
 	}
