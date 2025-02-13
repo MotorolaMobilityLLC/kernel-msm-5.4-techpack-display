b5503e9d7952e8e7843cb58850ac3ae2d9164ebc
daiss1 <daiss1@lenovo.com>
2025-02-13 17:26:14 +0800

    msm: Add true AOD backlight config
    Modify:
    Add cmd mode and video mode true AOD backlight config
    
    Change-Id: If2bf9a59c6d17b0a74ce8ffd8cb4985bcb99dfb1
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3234803
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display_mot_ext.h
+++ msm/dsi/dsi_display_mot_ext.h
@@ -79,5 +79,6 @@ static inline u32 dsi_display_mode_actual_rr(struct dsi_mode_info *timing)
 // END Motorola zhanggb, IKSWT-18219
 
 bool dsi_panel_is_gsi_mode(void);
+void dsi_panel_aod_backlight_update(struct dsi_panel *panel, enum dsi_cmd_set_type type);
 
 #endif /* _DSI_DISPLAY_MOT_EXT_H_ */
