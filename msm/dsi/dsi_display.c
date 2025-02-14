8b898564802e97a31459a91ae3c889837a40549b
daiss1 <daiss1@lenovo.com>
2025-02-14 18:11:37 +0800

    msm: panel AOD LP1 ignore esd check
    Modify:
    Panel AOD LP1 ignore esd check.
    Otherwise, ESD check fail under panel aod, cause black screen.
    (pick from https://gerrit.mot.com/3238500)
    
    Change-Id: I89c4e09ebbcfc9477fcaf7bd57a799f3cae045c0
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3295754
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -1072,6 +1072,11 @@ int dsi_display_check_status(struct drm_connector *connector, void *display,
 		return true;
 	}
 
+       if (panel->power_mode == SDE_MODE_DPMS_LP1) {
+		DSI_INFO("panel AOD LP1 ignore esd check\n");
+		return true;
+	}
+
 	dsi_panel_acquire_panel_lock(panel);
 
 	if (!panel->panel_initialized) {
