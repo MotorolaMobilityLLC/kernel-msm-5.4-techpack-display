fd44664296a43033a44cdf1dc81d2d07f94e851e
tianpan Chen <chentp1@motorola.com>
2025-03-28 11:37:48 +0800

    msm/sde: change bw if request bw exceeds 4.5Gbps
    Workaround solution for tm cli underrun:
    Change bw if request bw exceeds 4.5Gbps
    
    SR# 07584182
    base pick:https://gerrit.mot.com/#/c/3179828/
    fix underrun
    
    Change-Id: Iee5a118d9a8c436d3c114450a39218352bebd2dc
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3322209
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/sde/sde_core_perf.c
+++ msm/sde/sde_core_perf.c
@@ -783,6 +783,16 @@ static void _sde_core_perf_crtc_update_bus(struct sde_kms *kms,
 	bus_ab_quota = min(bus_ab_quota,
 			kms->catalog->perf.max_bw_high*1000ULL);
 
+	if (bus_ab_quota >= 4500000000ULL && strcmp(crtc->name, "crtc-1") == 0) {
+		if(bus_ab_quota + 2000000000ULL > kms->catalog->perf.max_bw_high*1000ULL){
+			bus_ab_quota = max(bus_ab_quota, kms->catalog->perf.max_bw_high*1000ULL);
+			SDE_INFO("change BW to Max value");
+		}else{
+			bus_ab_quota = bus_ab_quota + 2000000000ULL;
+			SDE_INFO("change BW to %llu value", bus_ab_quota);
+		}
+	}
+
 	if (kms->catalog->perf.num_ddr_channels && kms->catalog->perf.dram_efficiency) {
 		bus_ib_quota = div_u64(div_u64(bus_ab_quota,
 			kms->catalog->perf.num_ddr_channels) * 100,
