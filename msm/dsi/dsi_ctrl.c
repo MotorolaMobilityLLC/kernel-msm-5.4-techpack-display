8f55dbbe617fb8c87c96447f2bdf74da564b5f2f
wangyq13 <wangyq13@lenovo.com>
2025-04-01 11:58:16 +0800

    Revert "disp: msm: dsi: skip command scheduling during display disable"
    This reverts commit 9b47f497eefdd5628583027fd515c6a0dc5a11c0.
    New patch, drop for this CF
    Will be back after CF
    
    Mot-CRs-fixed: (CR)
    
    Signed-off-by: wangyq13 <wangyq13@lenovo.com>
    Change-Id: I8201c21200d0136a2bdd8a91aa5d36e36b623403
    Reviewed-on: https://gerrit.mot.com/3330893
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_ctrl.c
+++ msm/dsi/dsi_ctrl.c
@@ -1417,7 +1417,8 @@ static void dsi_kickoff_msg_tx(struct dsi_ctrl *dsi_ctrl,
 	 * HW never ‘sees’ another vblank line and hence it gets
 	 * stuck in the ‘wait’ state.
 	 */
-	if (flags & DSI_CTRL_CMD_CUSTOM_DMA_SCHED)
+	if ((flags & DSI_CTRL_CMD_CUSTOM_DMA_SCHED) ||
+		(dsi_ctrl->host_config.panel_mode == DSI_OP_VIDEO_MODE))
 		dsi_configure_command_scheduling(dsi_ctrl, cmd_mem);
 
 	dsi_ctrl->cmd_mode = (dsi_ctrl->host_config.panel_mode ==
