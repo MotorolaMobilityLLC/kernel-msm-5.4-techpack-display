bf1034b0efd6565919c0ffc0e864fdf6ecd0d585
tianpan Chen <chentp1@motorola.com>
2025-06-12 14:21:09 +0800

    SM8850 Urus display: add 8s code read config.
    For DDIC ili97680A boe panel, before read 0x80 register,
    need switch page 1D, otherwise will caused 8s read mistake,
    so distinguish with other panel, use three property to control:
    
    1.qcom,mdss-dsi-panel-cellid-send-command-enabled;
    //is it necessary to write commands to switch page?
    2.qcom,mdss-dsi-panel-cellid-send-command = [];
    //commands needed for switching pages for panel.
    3.qcom,mdss-dsi-panel-cellid-send-back-command = [];
    //commands needed for switch back to default page.
    
    Change-Id: If921cfcf26f33d9aed935e2f13521f65d4371d91
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427407
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -320,6 +320,8 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_DFPS_CMD_120:               DFPS switch to 120
  * @DSI_CMD_SET_DFPS_CMD_144:               DFPS switch to 144
  * @DSI_CMD_SET_PANEL_CELLID:               Panel cellid
+ * @DSI_CMD_SET_PANEL_SEND_CELLID:  panel cellid send write commands
+ * @DSI_CMD_SET_PANEL_SEND_CELLID_BACK: panel cellid send back commands
  * @DSI_CMD_SET_APL_ON:              apl on
  * @DSI_CMD_SET_APL_OFF:               apl off
  * @DSI_CMD_SET_PANEL_PCD_ENABLE:               Panel pcd check enable
@@ -395,6 +397,8 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_DFPS_CMD_120,
 	DSI_CMD_SET_DFPS_CMD_144,
 	DSI_CMD_SET_PANEL_CELLID,
+	DSI_CMD_SET_PANEL_SEND_CELLID,
+	DSI_CMD_SET_PANEL_SEND_CELLID_BACK,
 	DSI_CMD_SET_APL_ON,
 	DSI_CMD_SET_APL_OFF,
 	DSI_CMD_SET_PANEL_PCD_ENABLE,
