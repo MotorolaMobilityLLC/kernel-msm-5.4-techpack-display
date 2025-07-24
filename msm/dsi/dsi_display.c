f35877b784f12e6b7672037859c5bd13e66ba291
Guobin Zhang <zhanggb@motorola.com>
2025-07-24 14:30:41 +0800

    add sys node for calibration panel name
    Current panel name of device tree is different from the name content of
    /sys/class/drm/card0-DSI-X, which is same with bootloader panel name for
    certain motorola panel features.
    The panel name got by stc lib is the content of device tree, which is
    stored in a changeable debug sys node. We need fixed sysnode to read
    the panel name in device tree.
    Add a fixed sys node to store the device tree panel name, so we can load
    calibration param named with the panel name directly, while not need to
    parse the bootloader panel name and map to device tree panel name.
    
    Change-Id: I0d32a04e08d75eae9e263a3c275ed71aeadbf365
    Reviewed-on: https://gerrit.mot.com/3477898
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display.c
+++ msm/dsi/dsi_display.c
@@ -7275,6 +7275,28 @@ static ssize_t panelPcdCheck_show(struct device *device,
 	    return scnprintf(buf, PAGE_SIZE, "%s\n", "Not a DSI panel");
 }
 
+static ssize_t panelCalibrationName_show(struct device *device,
+	struct device_attribute *attr, char *buf)
+{
+	struct drm_connector *conn;
+	struct sde_connector *sde_conn;
+	struct dsi_display *dsi_display;
+
+	if (!device || !buf) {
+		SDE_ERROR("invalid input param(s)\n");
+		return -EAGAIN;
+	}
+
+	conn = dev_get_drvdata(device);
+	sde_conn = to_sde_connector(conn);
+	dsi_display = sde_conn->display;
+
+	if (sde_conn->connector_type == DRM_MODE_CONNECTOR_DSI)
+	    return scnprintf(buf, PAGE_SIZE, "%s\n", dsi_display->panel->name);
+	else
+	    return scnprintf(buf, PAGE_SIZE, "%s\n", "Not a DSI panel");
+}
+
 static DEVICE_ATTR_RO(panelId);
 static DEVICE_ATTR_RO(panelVer);
 static DEVICE_ATTR_RO(panelName);
@@ -7284,6 +7306,7 @@ static DEVICE_ATTR_RO(panelBLExponent);
 static DEVICE_ATTR_RO(panelCellId);
 static DEVICE_ATTR_RO(panelDC);
 static DEVICE_ATTR_RW(panelPcdCheck);
+static DEVICE_ATTR_RO(panelCalibrationName);
 
 static const struct attribute *sde_conn_panel_attrs[] = {
 	&dev_attr_panelId.attr,
@@ -7295,6 +7318,7 @@ static const struct attribute *sde_conn_panel_attrs[] = {
 	&dev_attr_panelCellId.attr,
 	&dev_attr_panelDC.attr,
 	&dev_attr_panelPcdCheck.attr,
+	&dev_attr_panelCalibrationName.attr,
 	NULL
 };
 
