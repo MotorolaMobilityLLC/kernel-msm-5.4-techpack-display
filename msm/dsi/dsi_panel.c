913d91749a0890543e880118e7b5de3ac19ca1d3
tianpan Chen <chentp1@motorola.com>
2025-09-22 18:12:45 +0800

    display/msm: modify 10-60-10 partial update config.
    Modify:
    StarShip panel is csot c10 which upgrade materials from c9,
    so can to support 10-60-10 partial update with lower mipiclock,
    based 60hz, register is different, so delete the driver config
    to keep consistent with different panel partial update dtsi.
    
    Change-Id: I5e70518474225f1f3d5159c8f68b2791969b63d4
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3551882
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -7197,18 +7197,6 @@ static void dsi_panel_partition_refreshrate_timming_switch_update(struct dsi_pan
 				cmds->msg.tx_len == 15){
                        payload[2] = payload[5] =payload[8] =payload[11] = payload[14] = dsi_display_prr_refreshrate_reg(panel->cur_partition_refreshrate.refreshrate3rd);
 		    }
-		}else if(refresh_rate == 60){
-		    //update refreshrate1st && update refreshrate3st
-		    if(payload[0] == 0x6B &&  ((panel->prr_config.boundaryLine_reg_count + 1) == i ||(panel->prr_config.boundaryLine_reg_count + 5) )&&
-				cmds->msg.tx_len == 15){
-				payload[2] = payload[5] =payload[8] =payload[11] = payload[14] = 0x0B;
-		    }
-
-		    //update refreshrate2st
-		    if(payload[0] == 0x6B &&  (panel->prr_config.boundaryLine_reg_count + 3) == i &&
-				cmds->msg.tx_len == 15){
-                           payload[2] = payload[5] =payload[8] =payload[11] = payload[14] = 0x01;
-		    }
 		}
 		cmds++;
 
