18415fbc09a4efcade34e18f5583455ba87ddaae
tianpan Chen <chentp1@motorola.com>
2025-10-23 15:41:28 +0800

    display/msm: add odc moto command function.
    Urus display/msm:
    for ili97680A boe panel, need to add ODC on/off config
    to optimize the motion blur effect in low backlight,
    greater than 1920. ODC off, less or equal 1920, ODC on.
    for example:
    qcom,odc-enabled;
    qcom,mdss-dsi-panel-ODC-THRESHOLD-BL = <1920>;
    qcom,mdss-dsi-panel-odc-on-command = [...];
    qcom,mdss-dsi-panel-odc-off-command = [...];
    
    Change-Id: I1445754642ea4d901e687baebef64b5673a0d176
    Signed-off-by: tianpan Chen <chentp1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3603111
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_defs.h
+++ msm/dsi/dsi_defs.h
@@ -324,6 +324,8 @@ enum dsi_dyn_clk_feature_type {
  * @DSI_CMD_SET_PANEL_SEND_CELLID_BACK: panel cellid send back commands
  * @DSI_CMD_SET_APL_ON:              apl on
  * @DSI_CMD_SET_APL_OFF:               apl off
+ * @DSI_CMD_SET_ODC_ON:              ODC on
+ * @DSI_CMD_SET_ODC_OFF:               ODC off
  * @DSI_CMD_SET_PANEL_PCD_ENABLE:               Panel pcd check enable
  * @DSI_CMD_SET_PANEL_PCD_DISABLE:               Panel pcd check disable
  * @DSI_CMD_SET_PANEL_PRR_ENABLE,                partition refreshrate ON
@@ -403,6 +405,8 @@ enum dsi_cmd_set_type {
 	DSI_CMD_SET_PANEL_SEND_CELLID_BACK,
 	DSI_CMD_SET_APL_ON,
 	DSI_CMD_SET_APL_OFF,
+	DSI_CMD_SET_ODC_ON,
+	DSI_CMD_SET_ODC_OFF,
 	DSI_CMD_SET_PANEL_PCD_ENABLE,
 	DSI_CMD_SET_PANEL_PCD_DISABLE,
 	DSI_CMD_SET_PANEL_PRR_ENABLE,
