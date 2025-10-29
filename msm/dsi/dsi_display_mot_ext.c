f738815374d79db3905faf192fa5a2d296b5e6f2
houdz1 <houdz1@lenovo.com>
2025-10-29 10:56:22 +0800

    dsi/panel: restore the backlight value to "bl_lvl_during_hbm"
    When enter AOD, will power off panel and set panel enter AOD mode, So the backligt
    is set to 0, so bl_lvl_during_hbm =0;
    When exit True AOD with FPS, set HBM to 0 and set backlight to "bl_lvl_during_hbm"
    That the backlight is 0, display is black.
    So Set "bl_lvl_during_hbm" to the right value to fix the issue.
    
    Change-Id: I58e4bbbf24a094c78b791e3db7538a3867534394
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/3613461
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Tested-by: Jira Key
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Reviewed-by: Jing Hu <hujing3@lenovo.com>
    Reviewed-by: <lvxj4@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_display_mot_ext.c
+++ msm/dsi/dsi_display_mot_ext.c
@@ -1817,6 +1817,7 @@ void dsi_panel_aod_backlight_update(struct dsi_panel *panel, enum dsi_cmd_set_ty
 					if(!panel->bl_config.bl_level) {
 						panel->bl_config.bl_level = bl_level;
 						panel->lhbm_config.dbv_level = bl_level;
+						panel->bl_lvl_during_hbm = bl_level;
 						pr_info("unexpect bl_level, need restore backlight and lhbm dbv from aod : %d\n", bl_level);
 					}
 					payload[1] = (bl_level & 0xFF00) >> 8;
