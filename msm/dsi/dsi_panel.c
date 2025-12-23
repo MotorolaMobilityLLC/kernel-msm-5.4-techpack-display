8eb32580f1681e1cacfbf53daff008d64e45bdee
Xiaojun Lv <lvxj4@motorola.com>
2025-12-23 20:35:10 +0800

    msm/dsi: optimize pcd sequence for vtdr6126a
    msm/dsi:
    For vtdr6126a, after 150ms delay, if pcd read not on D0 lane lp11 state,
    will return 0. Include this situation for vtdr6126a pcd sequence.
    
    Add prop "qcom,mdss-dsi-panel-pcd-value-zero_valid" to define.
    
    Change-Id: I70d1985f9e81ad90b58799a5a7d188c7f603f43b
    Reviewed-on: https://gerrit.mot.com/3695596
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pdo Scm <hudsoncm@motorola.com>
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -5234,6 +5234,7 @@ static int dsi_panel_parse_pcd_config(struct dsi_panel *panel)
 
 	pcd_config->pcd_value_2bytes = utils->read_bool(utils->data, "qcom,mdss-dsi-panel-pcd-value-2bytes");
 
+	pcd_config->pcd_value_zero_valid = utils->read_bool(utils->data, "qcom,mdss-dsi-panel-pcd-value-zero_valid");
 	rc = utils->read_u32(utils->data, "qcom,mdss-dsi-panel-pcd-reg-pass-min",
 		&(pcd_config->pcd_reg_pass_min));
 	if (rc)
@@ -7874,8 +7875,10 @@ void dsi_panel_parse_pcd_status(struct dsi_panel *panel) {
 				&& (panel->pcd_config.pcd_reg_val <= panel->pcd_config.pcd_reg_pass_max)) {
 			//valid reg
 			panel->pcd_config.pcd_reg_status = 1;
-		}
-		else {
+		} else if (panel->pcd_config.pcd_value_zero_valid && !panel->pcd_config.pcd_reg_val) {
+			//pcd value 0 vailid
+			panel->pcd_config.pcd_reg_status = 1;
+		} else {
 			//NG
 			pr_warn("%s: warn: abnormal pcd reg val:0x%02x, max:0x%02x, min:0x%02x\n", __func__,
 						panel->pcd_config.pcd_reg_val, panel->pcd_config.pcd_reg_pass_max, panel->pcd_config.pcd_reg_pass_min);
